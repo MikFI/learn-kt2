a4d12d9e18a010d6c687b282a0c913fb20d0b5f6
MikFI <vba12@mail.ru>
2022-09-15 16:23:47 +0700

    scrolling to the top fixed

--- app/src/main/java/ru/netology/nmedia/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/PostViewModel.kt
@@ -1,12 +1,74 @@
 package ru.netology.nmedia
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ru.netology.nmedia.repository.PostRepository
 import ru.netology.nmedia.repository.PostRepositoryInMemoryImpl
 
+//шаблон для создания нового поста
+//если ничего, кроме content не было передано - пост именно в таком виде и
+//улетит в хранилище (которое уже само бует разбираться, чего с этим делать)
+private val emptyPost: Post = Post(
+    id = 0,
+    author = "Me",
+    content = "",
+    published = "now"
+)
+
+//viewModel связывает UI с репозиторием(хранилищем) и отвечает за обработку того, что будет отрисовано в UI
+//UI просит данные для отрисовки из хранилища через этот класс
 class PostViewModel : ViewModel() {
     private val repository: PostRepository = PostRepositoryInMemoryImpl()
     val data = repository.getAll()
     fun likeById(id: Long) = repository.likeById(id)
     fun shareById(id: Long) = repository.shareById(id)
+
+    //tempPost - "несуществующий", временный пост, пользователь его не видит,
+    //заодно выступает классом транслирующим изменения себя всем, кто на него подписан
+    //подписчики (observe) видят изменения этой хтони и реагируют
+    //в нашем случае mainactivity реагирует на редактирование
+    val tempPost = MutableLiveData(emptyPost)
+
+    //сбрасываем содержимое поста после нажатия кнопки закрытия редактирования
+    //чтобы новое сообщение после этого не вставлялось непойми куда посреди ленты
+    fun cleanPostData(){
+        tempPost.value = emptyPost
+    }
+
+    //костыль для случая, когда кнопка "удалить" была нажата во время редактирования поста
+    //данная переменная хранит id удаляемого поста и как только происходит удаление, на это реагирует
+    //активити и зачищает поля ввода, если id совпало с тем, что находилось на редактировании
+    val deleted = MutableLiveData<Long>()
+
+    //суём во временный пост то, что нам отправили на редактирование
+    //потом оно будет изменено через changeContent--sendPost
+    fun edit(post: Post) {
+        tempPost.value = post
+    }
+
+    fun removeById(id: Long){
+        deleted.value = id
+        repository.removeById(id)
+    }
+
+    //создаём/обновляем пост (посылаем временный пост в репозиторий - он сам разберётся),
+    fun sendPost() {
+        tempPost.value?.let {
+            repository.save(it)
+        }
+        //зануляем пост после сохранения, чтобы в следующий пост,
+        //прошедший через эту функцию, не просочились поля из текущего поста
+        tempPost.value = emptyPost
+    }
+
+    //обновляем текст нашего временного поста
+    //если он отличается от того, что там лежит в данный момент
+    fun changeContent(text: String) {
+        if (text == tempPost.value?.content) {
+            return
+        }
+        tempPost.value = tempPost.value?.copy(content = text)
+    }
+
+
 }
--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -2,9 +2,15 @@ package ru.netology.nmedia.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
+import ru.netology.nmedia.AndroidUtils
+import ru.netology.nmedia.Post
 import ru.netology.nmedia.PostViewModel
 import ru.netology.nmedia.R
+import ru.netology.nmedia.adapter.OnInteractionListener
+import ru.netology.nmedia.adapter.PostAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,16 +28,120 @@ class MainActivity : AppCompatActivity() {
         //создаём viewModel, в которой будут производиться все действия с данными
         //здесь же, в activity, мы их только отображаем
         val viewModel: PostViewModel by viewModels()
+        val adapter = PostAdapter(listener = PostInteraction(viewModel))
 
-        val adapter = PostAdapter(
-            { post -> viewModel.likeById(post.id) },
-            { post -> viewModel.shareById(post.id) }
-        )
-
+        //получаем список постов и перематываем на самый верх в случае,
+        //если количество постов увеличилось на 1 в сравнении с прошлым обновлением
+        //(т.е. если только что добавили 1 пост)
         binding.postList.adapter = adapter
         viewModel.data.observe(this) { posts ->
-            adapter.submitList(posts)
+            val newPost = posts.size > adapter.currentList.size
+            adapter.submitList(posts) {
+                if (newPost) {
+                    binding.postList.scrollToPosition(0)
+                }
+            }
+        }
+
+        //id поста, находящегося в поле для редактирования
+        //(для реализации костыля, реагирующего на удаление поста во время редактирования)
+        var activePostId = 0L
+
+        //следим за "транслятором" - если он изменился (выбрано "редактировать" у поста и,
+        //соответственно, во "временном" посте появилось то, что нужно отредактировать),
+        //то включаем поле ввода, клавиатуру и копируем туда содержимое "временного" поста
+        //(при этом реагируем только на редактирование, а не на создание - т.е. id не должен быть передан)
+        viewModel.tempPost.observe(this) {
+            if (it.id == 0L) {
+                return@observe
+            }
+
+            activePostId = it.id
+            binding.apply {
+                newContent.setText(it.content)
+                actionText.text = it.content
+                groupEditAction.visibility = View.VISIBLE
+                actionTypeCreate.visibility = View.GONE
+                newContent.requestFocus()
+                AndroidUtils.showKeyboard(newContent)
+            }
+        }
+
+        //если пост удаляется во время редактирования,
+        //то все поля очищаются и фокус ввода сбрасывается
+        viewModel.deleted.observe(this) {
+            if (it == activePostId) {
+                binding.apply {
+                    newContent.setText("")
+                    newContent.clearFocus()
+                    groupEditAction.visibility = View.GONE
+                    AndroidUtils.hideKeyboard(newContent)
+                }
+                viewModel.cleanPostData()
+            } else {
+                return@observe
+            }
+        }
+
+        //обработчик нажатия кнопки "отправить", что рядом с полем ввода
+        binding.buttonSendPost.setOnClickListener {
+            binding.apply {
+                val text = newContent.text?.toString()?.trim()
+                if (text.isNullOrBlank()) {
+                    val emptyString = resources.getString(R.string.empty_content_error)
+                    Toast.makeText(newContent.context, emptyString, Toast.LENGTH_SHORT).show()
+                    return@setOnClickListener
+                }
+                viewModel.changeContent(text)
+                viewModel.sendPost()
+                newContent.setText("")
+                newContent.clearFocus()
+                groupEditAction.visibility = View.GONE
+                AndroidUtils.hideKeyboard(newContent)
+            }
+        }
+
+        //обработчик кнопки "закрыть" в менюшке над полем ввода
+        binding.buttonCancelEdit.setOnClickListener {
+            binding.apply {
+                newContent.setText("")
+                newContent.clearFocus()
+                groupEditAction.visibility = View.GONE
+                AndroidUtils.hideKeyboard(newContent)
+            }
+            viewModel.cleanPostData()
+        }
+
+        //отображение корректного текста (создание/редактирование сообщения)
+        //в менюшке над полем ввода
+        binding.newContent.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                binding.apply {
+                    groupEditAction.visibility = View.VISIBLE
+                    actionTypeEdit.visibility = View.GONE
+                    actionText.visibility = View.GONE
+                }
+            }
         }
     }
+}
 
+class PostInteraction(viewModel: PostViewModel) : OnInteractionListener {
+    private val v = viewModel
+
+    override fun like(post: Post) {
+        v.likeById(post.id)
+    }
+
+    override fun share(post: Post) {
+        v.shareById(post.id)
+    }
+
+    override fun edit(post: Post) {
+        v.edit(post)
+    }
+
+    override fun remove(post: Post) {
+        v.removeById(post.id)
+    }
 }
